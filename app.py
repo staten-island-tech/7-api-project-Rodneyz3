21649a39116d46ba19a34f93a29080a0bcdae131
Rodneyz3 <ROdneyz3@nycstudents.net>
2025-12-10 09:26:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import requests
 
-def api(reciple):
-    response = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?query=pizza&apiKey=17b0dfdac1e6454d9f3bc9e4cb27b568{reciple.lower()}")
+def getPoke(poke):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+
+pokemon = getPoke("raichu")
+print(pokemon)
